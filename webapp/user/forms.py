750a6272f68c21142a9eeff15ab1d9920b8625e4
Stepabov Nikolai <Data.Science.NikSte@yandex.ru>
2020-10-27 17:54:43 +0300

    Добавленна регистрация пользователей

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -2,10 +2,17 @@ from flask_wtf import FlaskForm # we need it for creating a form
 #give us fields for Username, Password, and submition
 from wtforms import StringField, BooleanField ,PasswordField, SubmitField
 #check that user send a date
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class Loginform(FlaskForm):
     username = StringField('User name', validators=[DataRequired()], render_kw={'class':"form-control"})
     password = PasswordField('Password', validators=[DataRequired()], render_kw={'class':"form-control"})
     remember_me = BooleanField("Remember me", default=True ,render_kw={'class': 'form-check-input'})
+    submit = SubmitField('Send', render_kw={'class':'btn btn-primary'})
+    
+class RegistrationForm(FlaskForm):
+    username = StringField('User name', validators=[DataRequired()], render_kw={'class':"form-control"})
+    email = StringField('Email', validators=[DataRequired(), Email()], render_kw={'class':"form-control"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={'class':"form-control"})
+    password2 = PasswordField('Write Password again', validators=[DataRequired(), EqualTo('password')], render_kw={'class':"form-control"})
     submit = SubmitField('Send', render_kw={'class':'btn btn-primary'})
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user
 
-from webapp.user.forms import Loginform
+from webapp.db import db
+from webapp.user.forms import Loginform, RegistrationForm
 from webapp.user.models import User
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
@@ -35,3 +36,24 @@ def logout():
     logout_user()
     flash("See you later, space cowboy")
     return redirect(url_for('news.index'))
+
+@blueprint.route('/register')
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('news.index'))
+    form = RegistrationForm()
+    title = "Регистрация"
+    return render_template('user/registration.html', page_title=title, form=form)
+
+@blueprint.route('/process_reg', methods=['POST'])
+def process_reg():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        news_user = User(username=form.username.data, email=form.email.data, role='user')
+        news_user.set_password(form.password.data)
+        db.session.add(news_user)
+        db.session.commit()
+        flash('You have successfully registered')
+        return redirect(url_for('user.login'))
+    flash('Please fix errors')
+    return redirect(url_for('user/registration.html'))
